b23f1c43cf3dccdc1a4eb124b7f38c3cb946e15d
Benjamin Miño <natanael.96.01@gmail.com>
2023-03-09 12:56:27 -0300

    added post route of Cart

--- src/routes/controllers/Cart/index.js
+++ src/routes/controllers/Cart/index.js
@@ -4,6 +4,11 @@ const { Cart, Product, User, Book } = require("../../../db");
 module.exports = {
     createCart: async (total_products) => {
         if (!total_products || !total_products.length) throw new Error("Necesita productos para iniciar un carrito.");
-        
+        let total_amount = total_products.reduce((ac,prod)=> ac + prod.quantity * prod.price, 0);
+        const cart = await Cart.create({
+            total_amount,
+            total_products
+        });
+        return cart;
     },
 }
